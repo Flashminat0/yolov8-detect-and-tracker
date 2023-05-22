9f95cf21f78231a7840aa819eaeee253f10508f8
Yasas Lowe <it20014940@my.sliit.lk>
2023-05-23 02:07:01 +0530

    now all files will sent to firebase storage before coming back on response

--- capture_to_find.py
+++ capture_to_find.py
@@ -1,6 +1,10 @@
+import os
+
 from crop_active import run_tracking
 from similarity_checker import compare_laptop_images
 import cv2 as cv
+
+from storage_service import StorageService
 
 
 def capture_to_find(image_url):
@@ -16,18 +20,34 @@ def capture_to_find(image_url):
         f'output/exp{exp}/crops/laptop', exp)
 
     # Display the images with the maximum similarity for each laptop
+    laptop_list = []
+    storage = StorageService()
+
     for laptop, values in data.items():
         max_img_path = values['max_img_path']
         img = cv.imread(max_img_path)
-        # cv.imshow(f'Laptop {laptop}', img)
+        # extract image name from path
+        img_name = os.path.basename(max_img_path)
+        # upload to Firebase
+        upload_response = storage.upload_file(max_img_path, img_name)
+        # get URL of uploaded image
+        img_url = storage.get_file_url(img_name)
+        # update values with new URL
+        values['max_img_path'] = img_url
+        laptop_list.append(values)
 
-    # cv.waitKey(0)
-    # cv.destroyAllWindows()
+    # Upload the frame image
+    frame_img_path = f"output\\exp{exp}\\0.jpg"
+    frame_img_name = os.path.basename(frame_img_path)
+    storage.upload_file(frame_img_path, frame_img_name)
+    frame_url = storage.get_file_url(frame_img_name)
 
-    # similarities = compare_with_max_images(
-    #     'https://firebasestorage.googleapis.com/v0/b/research-cctv.appspot.com/o/Untitled.png?alt=media&token=33fbcaf0-5697-42ec-908d-2be212f8fd0b',
-    #     data, exp)
+    response_data = {
+        "laptops": laptop_list,
+        "reference_image": image_url,
+        "frame": frame_url
+    }
 
-    return data
+    return response_data
 
 # print(similarities)
